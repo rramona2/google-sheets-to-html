e566b4e5dc0451ed4fe4cf7b03dd8e33ffcf13f3
Ryan Ramona <rramona2@users.noreply.github.com>
2017-11-14 23:22:42 -0800

    Update google-sheets-html.js

--- js/google-sheets-html.js
+++ js/google-sheets-html.js
@@ -15,7 +15,7 @@ google.load('visualization', '1', {
 var visualization;
 
 function drawVisualization() {
-    var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1y8QRGUT0bb0Wx6lAHbMjNxU7Zs96WDJE9SXRiuQQfJc&output=html&usp=sharing');
+    var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1yYPu19q3f7tsOYnOtL_Wq2lgQpW9wFNtJytaU-L9FMk&output=html&usp=sharing');
     query.setQuery('SELECT A, B, C, D label A "Duration", B "Song", C "Requested By", D "URL"');
     query.send(handleQueryResponse);
 }
